2623e58e128608179494315c65fe0335f6aebdca
徐乔伦 <Jamesxql@Gmail.com>
2017-11-30 16:50:18 +0800

    add some test cases

---
+++ test/FeiniuBus.Grpc.Hosting.Tests/Fakes/CountStartup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FeiniuBus.Grpc.Hosting.Tests.Fakes
+{
+    public class CountStartup
+    {
+        public static int ConfigureServicesCount;
+        public static int ConfigureCount;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureServicesCount++;
+        }
+
+        public void Configure(IApplicationBuilder builder)
+        {
+            ConfigureCount++;
+        }
+    }
+}
---
+++ test/FeiniuBus.Grpc.Hosting.Tests/GrpcHostTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using FeiniuBus.Grpc.Hosting.Tests.Fakes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace FeiniuBus.Grpc.Hosting.Tests
+{
+    public class GrpcHostTests
+    {
+        [Fact]
+        public void EnvDefaultsToProductionIfNoConfig()
+        {
+            using (var host = new GrpcHostBuilder().UseStartup<Fakes.Startup>().Build())
+            {
+                var env = host.Services.GetService<IHostingEnvironment>();
+                Assert.Equal(EnvironmentName.Production, env.EnvironmentName);
+            }
+        }
+
+        [Fact]
+        public void IsEnvironment_Extension_Is_Case_Insensitive()
+        {
+            using (var host = new GrpcHostBuilder().UseStartup<Fakes.Startup>().Build())
+            {
+                host.Start();
+                var env = host.Services.GetService<IHostingEnvironment>();
+                Assert.True(env.IsEnvironment(EnvironmentName.Production));
+                Assert.True(env.IsEnvironment("producTion"));
+            }
+        }
+
+        [Fact]
+        public void EnvDefaultsToConfigValueIfSpecified()
+        {
+            var vals = new Dictionary<string, string>
+            {
+                {"Environment", EnvironmentName.Staging}
+            };
+
+            var builder = new ConfigurationBuilder().AddInMemoryCollection(vals);
+            var config = builder.Build();
+
+            using (var host = new GrpcHostBuilder().UseConfiguration(config).UseStartup<Fakes.Startup>().Build())
+            {
+                var env = host.Services.GetService<IHostingEnvironment>();
+                Assert.Equal(EnvironmentName.Staging, env.EnvironmentName);
+            }
+        }
+
+        [Fact]
+        public void WebHost_InvokesConfigureMethodsOnlyOnce()
+        {
+            using (var host = new GrpcHostBuilder().UseStartup<CountStartup>().Build())
+            {
+                host.Start();
+                Assert.Equal(1, CountStartup.ConfigureServicesCount);
+                Assert.Equal(1, CountStartup.ConfigureCount);
+            }
+        }
+    }
+}
