0b6847f07a63b103372430189a612b7c71f39112
徐乔伦 <Jamesxql@Gmail.com>
2017-11-30 15:10:43 +0800

    add some test cases

---
+++ test/FeiniuBus.Grpc.Hosting.Tests/Fakes/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FeiniuBus.Grpc.Hosting.Tests.Fakes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        public void Configure(IApplicationBuilder builder)
+        {
+            
+        }
+    }
+}
---
+++ test/FeiniuBus.Grpc.Hosting.Tests/GrpcHostBuilderTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace FeiniuBus.Grpc.Hosting.Tests
+{
+    public class GrpcHostBuilderTests
+    {
+        [Fact]
+        public void ApplicationLifetimeRegistered()
+        {
+            var builder = CreateGrpcHostBuilder();
+            var host = builder.Build();
+            using (host)
+            {
+                host.Start();
+                var services = host.Services.GetServices<IApplicationLifetime>();
+                Assert.NotNull(services);
+                Assert.NotEmpty(services);
+            }
+        }
+
+        [Fact]
+        public void HostConfigurationBuilde()
+        {
+            var settings = new Dictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"}
+            };
+            var config = new ConfigurationBuilder().AddInMemoryCollection(settings).Build();
+            var builder = CreateGrpcHostBuilder().UseConfiguration(config);
+            var host = builder.Build();
+            using (host)
+            {
+                host.Start();
+                var configuration = host.Services.GetRequiredService<IConfiguration>();
+                Assert.NotNull(configuration);
+                Assert.Equal("value1", configuration["key1"]);
+                Assert.Equal("value2", configuration["key2"]);
+            }
+        }
+
+        [Fact]
+        public void HostEndpointsRegistered()
+        {
+            var builder = CreateGrpcHostBuilder().UseUrls("0.0.0.0:4099");
+            var host = builder.Build();
+            using (host)
+            {
+                host.Start();
+                Assert.NotEmpty(host.Server.Ports);
+                Assert.Equal("0.0.0.0", host.Server.Ports.First().Host);
+                Assert.Equal(4099, host.Server.Ports.First().Port);
+            }
+        }
+        
+        private IGrpcHostBuilder CreateGrpcHostBuilder()
+        {
+            return new GrpcHostBuilder().UseStartup<Fakes.Startup>();
+        }
+    }
+}
